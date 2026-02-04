70153f266f9c7f3efb24f00cc38fc7dc6801535a
elantoinebasset <antoine.basset17@orange.fr>
2026-02-04 16:27:18 +0100

    Switch, Match

--- Control_Structure.php
+++ Control_Structure.php
@@ -19,6 +19,11 @@ else {
 
 echo "<br>";
 // Les switchs
+/*Un SWITCH est une structure de contrôle qui permet de comparer une variable
+à plusieurs valeurs possibles et d'exécuter du code différent selon le cas.
+C'est une alternative plus lisible aux multiples if/elseif quand on teste
+la même variable contre plusieurs valeurs.
+*/
 
 switch ($a) {
     case 1:
@@ -29,3 +34,21 @@ switch ($a) {
         break;
     default: "Aucuns des cas n'est correct";
 }
+
+
+echo "<br>";
+// Les Matchs
+
+
+$favcolor = "chien"; //Le match recherche la donnée ecrite ici qui peux être ensuite choisi dans la variable 
+
+$text = match($favcolor) {
+  "red" => "Your favorite color is red!",
+  "blue" => "Your favorite color is blue!",
+  "green" => "Your favorite color is green!",
+  "chien" => "le chien est heureux",
+  1,2,3=> "un chiffre", //ATTENTION les chiffres ne sont pas entre "" donc si je veux qu'ils soient choisis je dois faire $favcolor = 1
+  default => "Your favorite color is neither red, blue, nor green, nor the chien!"
+};
+//POUR LE MATCH LA VIRGULE EST IMPORTANTE , pour TOUTES LES PHRASES sauf la derniere.
+echo $text;
