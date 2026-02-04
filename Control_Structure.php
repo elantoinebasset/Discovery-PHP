8323c1864c71460551a2597a08bbf3b7d83d8c50
elantoinebasset <antoine.basset17@orange.fr>
2026-02-04 12:58:37 +0100

    Switch Added

--- Control_Structure.php
+++ Control_Structure.php
@@ -9,4 +9,23 @@ if ($a < $b && !$bool) {
 echo "la premiere condition est vrai";
 } else if ($bool) {
     echo "la deuxieme est vrai";
+} 
+// les else if peuvent être stackés les unes aprés les autres car tant qu'il y en a pas une qui est juste alors elles continuent
+
+else {
+    echo "elles sont toutes mauvaises";
+} 
+
+
+echo "<br>";
+// Les switchs
+
+switch ($a) {
+    case 1:
+        echo "le premier cas est correct";
+        break;
+    case 3:
+        echo "le deuxieme cas est correct";
+        break;
+    default: "Aucuns des cas n'est correct";
 }
